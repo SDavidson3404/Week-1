c6d4c2d5a5899c7b30fa40ad58b33b2ec4181e16
SDavidson3404 <bd1127702@student.francistuttle.edu>
2025-08-25 14:35:21 -0500

    Work Added
    Added work

---
+++ C#.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("________");
+Console.WriteLine("Line 1");
+Console.WriteLine("Line 2");
+Console.WriteLine("Line 3");
+Console.WriteLine("________");
+Console.WriteLine(2 + 2);
+Console.WriteLine(10 - 5);
+Console.WriteLine(3 * 4);  
+Console.WriteLine("________");
+Console.WriteLine("*****");
+Console.WriteLine("*****");
+Console.WriteLine("*****");
+Console.WriteLine("________");
+Console.WriteLine("The answer is: 42");
---
+++ Python.py
@@ -0,0 +1,21 @@
+x = 2
+y = 10
+z = 5
+a = 3
+b = 4
+
+print("Hello, World!")
+print("_____________")
+print("Line 1")
+print("Line 2")
+print("Line 3")
+print("_____________")
+print(x + x)
+print(y - z)
+print(a * b)
+print("_____________")
+print("*****")
+print("*****")
+print("*****")
+print("_____________")
+print("The answer is: 42")
